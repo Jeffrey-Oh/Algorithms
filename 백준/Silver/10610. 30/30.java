ea808bf4e10db95705d1d5972e8924c899599143
오태현 <jeffreyoh121@gmail.com>
2024-08-19 00:19:41 +0900

    Update 30.java

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -12,24 +12,22 @@ public class Main {
         String N = br.readLine();
         StringBuilder result = new StringBuilder();
 
-        // Convert the input number into a list of digits
-        List<Integer> list = Arrays.stream(N.split("")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        // 30 배수 중 가장 큰 수
+        List<Integer> list = Arrays.stream(N.split("")).mapToInt(Integer::parseInt).boxed().sorted(Collections.reverseOrder()).collect(Collectors.toList());
 
-        // Check if the sum of the digits is divisible by 3
+        // 3의 배수는 모든 자리수를 더해도 3의 배수여야함
         int sum = list.stream().mapToInt(Integer::intValue).sum();
 
-        // Check if the number contains at least one '0'
-        if (sum % 3 != 0 || !list.contains(0)) {
+        // 10의 배수를 확인하기 위해 0이 적어도 1개 있어야함
+        if (sum % 3 != 0 || !N.contains("0")) {
             bw.write("-1\n");
-        } else {
-            // Sort the digits in descending order
-            list.sort(Collections.reverseOrder());
-            for (Integer integer : list) {
-                result.append(integer);
-            }
-            bw.write(result.toString() + "\n");
+            bw.flush();
+            return;
         }
 
+        for (Integer integer : list) result.append(integer);
+
+        bw.write(result + "\n");
         bw.flush();
     }
 }
