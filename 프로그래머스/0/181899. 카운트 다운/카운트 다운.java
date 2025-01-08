5cef69e6d09451e0100277668eae22ec7d7958db
오태현 <jeffreyoh121@gmail.com>
2025-01-09 00:36:34 +0900

    [level 0] Title: 카운트 다운, Time: 3.62 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,9 +1,7 @@
+import java.util.stream.*;
+
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        int size = start_num - end_num + 1;
-        int[] answer = new int[size];
-        for (int i=0; i<size; i++)
-            answer[i] = start_num--;
-        return answer;
+        return IntStream.rangeClosed(-start_num, -end_num).map(i -> -i).toArray();
     }
 }
