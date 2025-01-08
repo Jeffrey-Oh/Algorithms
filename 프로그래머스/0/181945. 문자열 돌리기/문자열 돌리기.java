273227cc5b59dfaab5ab62050c89967ee1be20d0
오태현 <jeffreyoh121@gmail.com>
2025-01-09 01:31:16 +0900

    [level 0] Title: 문자열 돌리기, Time: 169.88 ms, Memory: 64.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] arr = sc.next().split("");
+        for (String str : arr)
+            System.out.println(str);
+    }
+}
