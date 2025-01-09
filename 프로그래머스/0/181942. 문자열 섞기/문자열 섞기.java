dcfb3b54a09c04a338563308a8c89729f0c1997a
오태현 <jeffreyoh121@gmail.com>
2025-01-09 17:20:11 +0900

    [level 0] Title: 문자열 섞기, Time: 0.05 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,10 +1,11 @@
 class Solution {
     public String solution(String str1, String str2) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        char[] s1 = str1.toCharArray();
+        char[] s2 = str2.toCharArray();
         for (int i=0; i<str1.length(); i++) {
-            answer += str1.charAt(i);
-            answer += str2.charAt(i);
+            sb.append(s1[i]).append(s2[i]);
         }
-        return answer;
+        return sb.toString();
     }
 }
