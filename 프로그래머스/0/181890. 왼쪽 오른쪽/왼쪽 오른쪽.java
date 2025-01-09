396cc500d7b6e91b4ccf93f74e38abbfd173c3ef
오태현 <jeffreyoh121@gmail.com>
2025-01-09 23:01:27 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.05 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -2,17 +2,13 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] str_list) {
-        String[] answer = {};
-        List<String> list = new ArrayList<>();
         for (int i=0; i<str_list.length; i++) {
             if (str_list[i].equals("l")) {
-                for (int j=0; j<i; j++) list.add(str_list[j]);
-                break;
+                return Arrays.copyOfRange(str_list, 0, i);
             } else if (str_list[i].equals("r")) {
-                for (int j=i+1; j<str_list.length; j++) list.add(str_list[j]);
-                break;
+                return Arrays.copyOfRange(str_list, i + 1, str_list.length);
             }
         }
-        return list.stream().toArray(String[]::new);
+        return new String[0];
     }
 }
