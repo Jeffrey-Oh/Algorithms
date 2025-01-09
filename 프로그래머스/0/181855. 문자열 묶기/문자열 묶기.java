f631a712253c443695431153e02b41530554374b
오태현 <jeffreyoh121@gmail.com>
2025-01-09 20:38:44 +0900

    [level 0] Title: 문자열 묶기, Time: 23.49 ms, Memory: 124 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] strArr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (String str : strArr) {
+            map.put(str.length(), map.getOrDefault(str.length(), 0) + 1);
+        }
+        int max = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            max = Math.max(max, entry.getValue());
+        }
+        return max;
+    }
+}
