f7c8662101b3d053200ee9101241892167db6576
오태현 <jeffreyoh121@gmail.com>
2025-01-09 15:13:47 +0900

    [level 0] Title: 날짜 비교하기, Time: 1.02 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -4,7 +4,7 @@ class Solution {
     public int solution(int[] date1, int[] date2) {
         LocalDate date11 = LocalDate.of(date1[0], date1[1], date1[2]);
         LocalDate date22 = LocalDate.of(date2[0], date2[1], date2[2]);
-        return date11.isEqual(date22) ? 0 : date11.isBefore(date22) ? 1 : 0;
+        return date11.isBefore(date22) ? 1 : 0;
         
     }
 }
