79e79645803796a2ec6b2a482df3afe4fe8f38b6
오태현 <jeffreyoh121@gmail.com>
2025-01-09 22:43:49 +0900

    [level 0] Title: 배열 만들기 6, Time: 46.00 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -2,12 +2,11 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
-        List<Integer> list = new ArrayList<>();
-        for (int i=0; i<arr.length; i++) {
-            if (list.isEmpty() || list.get(list.size() - 1) != arr[i]) list.add(arr[i]);
-            else if (list.get(list.size() - 1) == arr[i]) list.remove(list.size() - 1);
+        Stack<Integer> stack = new Stack<>();
+        for (int num : arr) {
+            if (!stack.isEmpty() && stack.peek() == num) stack.pop();
+            else stack.push(num);
         }
-        if (list.isEmpty()) list.add(-1);
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        return stack.isEmpty() ? new int[]{-1} : stack.stream().mapToInt(Integer::intValue).toArray();
     }
 }
