27de14ec52f2dfdacf412dd0e6cefeba7c1c7b00
오태현 <jeffreyoh121@gmail.com>
2025-01-09 16:48:54 +0900

    [level 0] Title: 글자 지우기, Time: 17.84 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -2,11 +2,9 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string, int[] indices) {
-        Arrays.sort(indices);
-        int idx = 0;
         for (int indice : indices) {
-            my_string = my_string.substring(0, indice-idx) + my_string.substring(indice+1-idx++);
+            my_string = my_string.substring(0, indice) + " " + my_string.substring(indice+1);
         }
-        return my_string;
+        return my_string.replace(" ", "");
     }
 }
