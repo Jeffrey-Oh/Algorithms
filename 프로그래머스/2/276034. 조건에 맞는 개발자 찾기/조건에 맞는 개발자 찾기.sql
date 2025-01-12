913157eeff55c9e73ef2a3cd53651d1834654dd1
오태현 <jeffreyoh121@gmail.com>
2025-01-13 01:40:11 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,8 @@
+-- 코드를 작성해주세요
+select id, email, first_name, last_name
+from developers d
+where (
+    (skill_code & (select code from skillcodes where name = 'C#'))
+    or (skill_code & (select code from skillcodes where name = 'Python'))
+)
+order by id
