4710b0c95d62b7f72382f7b9ad54825de8df2876
오태현 <jeffreyoh121@gmail.com>
2025-01-12 15:12:55 +0900

    [level 2] Title: [1차] 캐시, Time: 60.19 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -6,16 +6,16 @@ class Solution {
         int answer = 0;
         for (String city : cities) {
             city = city.toLowerCase();
+            
+            if (!ad.isEmpty() && ad.contains(city)) answer++;
+            else answer += 5;
+            
             if (ad.size() < cacheSize) {
-                if (ad.size() != 0 && ad.contains(city)) answer++;
-                else answer += 5;
                 ad.addLast(city);
                 continue;
             }
             
-            if (ad.size() != 0 && ad.contains(city)) answer++;
-            else answer += 5;
-            if (ad.size() != 0) {
+            if (!ad.isEmpty()) {
                 if (ad.contains(city)) {
                     ad.remove(city);
                 } else {
