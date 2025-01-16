ce3eb262dfc3b72d3ecd76eca3a019802ac2af10
오태현 <jeffreyoh121@gmail.com>
2025-01-16 12:25:19 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 48.27 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        ArrayDeque<Integer> ad = new ArrayDeque<>();
+        for (int num : arr) {
+            if (ad.isEmpty())
+                ad.add(num);
+            else if (ad.peekLast() != num) {
+                ad.add(num);
+            }
+        }
+        return ad.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
