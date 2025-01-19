a59acac73d1bcd6a3af510d9ffb9010889ad8cbe
오태현 <jeffreyoh121@gmail.com>
2025-01-19 17:21:18 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 13.06 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -4,39 +4,44 @@ class Solution {
     public int solution(int n, int[][] wires) {
         Map<Integer, LinkedHashSet<Integer>> graph = new HashMap<>();
         for (int[] wire : wires) {
-            graph.computeIfAbsent(wire[0], g -> new LinkedHashSet<>()).add(wire[1]);
-            graph.computeIfAbsent(wire[1], g -> new LinkedHashSet<>()).add(wire[0]);
+            graph.put(wire[0], new LinkedHashSet<>());
+            graph.put(wire[1], new LinkedHashSet<>());
+        }
+        for (int[] wire : wires) {
+            graph.get(wire[0]).add(wire[1]);
+            graph.get(wire[1]).add(wire[0]);
         }
         
         int min = Integer.MAX_VALUE;
         
         for (int[] wire : wires) {
-            int node1 = wire[0];
-            int node2 = wire[1];
-            
-            graph.get(node1).remove(node2);
-            graph.get(node2).remove(node1);
+            graph.get(wire[0]).remove(wire[1]);
+            graph.get(wire[1]).remove(wire[0]);
             
             boolean[] visited = new boolean[n + 1];
-            int subSize = dfs(graph, node1, visited);
+            int subSize = dfs(graph, wire[0], visited);
             
             min = Math.min(Math.abs((n - subSize) - subSize), min);
             
-            graph.get(node1).add(node2);
-            graph.get(node2).add(node1);
+            graph.get(wire[0]).add(wire[1]);
+            graph.get(wire[1]).add(wire[0]);
         }
+        
         return min;
     }
     
-    public int dfs(Map<Integer, LinkedHashSet<Integer>> graph, int node, boolean[] visited) {
-        visited[node] = true;
-        int size = 1;
+    public int dfs(Map<Integer, LinkedHashSet<Integer>> graph, int start, boolean[] visited) {
+        visited[start] = true;
+        int sum = 1;
         
-        for (int n : graph.get(node)) {
-            if (!visited[n])
-                size += dfs(graph, n, visited);
+        for (int num : graph.get(start)) {
+            if (!visited[num]) {
+                visited[num] = true;
+                sum += dfs(graph, num, visited);
+                visited[num] = false;
+            }
         }
         
-        return size;
+        return sum;
     }
 }
