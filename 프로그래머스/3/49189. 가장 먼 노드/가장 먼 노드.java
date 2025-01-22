110ded01c493fb1142ac1c7f51f33b7b9dded8de
오태현 <jeffreyoh121@gmail.com>
2025-01-22 15:14:33 +0900

    [level 3] Title: 가장 먼 노드, Time: 76.90 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -3,17 +3,16 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] edge) {
         Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int i=1; i<=n; i++) {
-            graph.put(i, new ArrayList<>());
-        }
         for (int[] e : edge) {
-            graph.get(e[0]).add(e[1]);
-            graph.get(e[1]).add(e[0]);
+            graph.computeIfAbsent(e[0], l -> new ArrayList<>()).add(e[1]);
+            graph.computeIfAbsent(e[1], l -> new ArrayList<>()).add(e[0]);
         }
+        
         int[] distances = bfs(graph, n, 1);
         
         int max = Arrays.stream(distances).max().orElse(0);
-        return (int) Arrays.stream(distances).filter(c -> c == max).count();
+
+        return (int) Arrays.stream(distances).filter(i -> i == max).count();
     }
     
     public int[] bfs(Map<Integer, List<Integer>> graph, int n, int start) {
@@ -21,16 +20,16 @@ class Solution {
         Arrays.fill(distances, -1);
         distances[start] = 0;
         
-        ArrayDeque<Integer> ad = new ArrayDeque<>();
-        ad.add(start);
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        q.add(start);
         
-        while (!ad.isEmpty()) {
-            int node = ad.pop();
+        while (!q.isEmpty()) {
+            int node = q.pop();
             
-            for (int neighbor : graph.get(node)) {
-                if (distances[neighbor] == -1) {
-                    distances[neighbor] = distances[node] + 1;
-                    ad.add(neighbor);
+            for (int num : graph.get(node)) {
+                if (distances[num] == -1) {
+                    distances[num] = distances[node] + 1;
+                    q.add(num);
                 }
             }
         }
