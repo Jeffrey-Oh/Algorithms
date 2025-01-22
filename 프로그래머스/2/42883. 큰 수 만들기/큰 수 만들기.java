98dc4c0b51c9910e110dce62f7f6276c1f825118
오태현 <jeffreyoh121@gmail.com>
2025-01-22 16:10:39 +0900

    [level 2] Title: 큰 수 만들기, Time: 59.56 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,32 +1,25 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 
 class Solution {
     public String solution(String number, int k) {
-        ArrayDeque<Character> stack = new ArrayDeque<>(); // 스택 역할
-
-        for (char digit : number.toCharArray()) {
-            // 스택의 최상단 숫자가 현재 숫자보다 작고, 제거할 기회(k)가 남아 있다면 제거
-            while (!stack.isEmpty() && stack.peekLast() < digit && k > 0) {
-                stack.pollLast(); // 스택에서 제거
-                k--; // 제거 기회 소진
+        ArrayDeque<Character> ad = new ArrayDeque<>();
+        
+        for (char c : number.toCharArray()) {
+            while (!ad.isEmpty() && ad.peekLast() < c && k > 0) {
+                ad.pollLast();
+                k--;
             }
-
-            stack.addLast(digit); // 현재 숫자를 스택에 추가
+            ad.add(c);
         }
-
-        // 스택에서 상위 k개의 숫자를 제거 (필요 없는 경우)
+        
         while (k > 0) {
-            stack.pollLast();
+            ad.pollLast();
             k--;
         }
-
-        // 스택의 내용을 문자열로 변환
-        StringBuilder result = new StringBuilder();
-        for (char c : stack) {
-            result.append(c);
-        }
-
-        return result.toString();
+        
+        StringBuilder answer = new StringBuilder();
+        for (char c : ad) answer.append(c);
+        
+        return answer.toString();
     }
 }
