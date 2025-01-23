b27754044243ca7f6c65314a146894ede4c3d6e8
오태현 <jeffreyoh121@gmail.com>
2025-01-23 22:10:30 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.87 ms, Memory: 56.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] maps) {
+        int n = maps.length;
+        int m = maps[0].length;
+        
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {-1, 1, 0, 0};
+        
+        Deque<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{0, 0});
+        maps[0][0] = 1;
+        
+        while (!q.isEmpty()) {
+            int[] current = q.poll();
+            int x = current[0];
+            int y = current[1];
+            
+            if (x == n - 1 && y == m - 1) {
+                return maps[x][y];
+            }
+            
+            for (int i=0; i<4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] == 1) {
+                    maps[nx][ny] = maps[x][y] + 1;
+                    q.add(new int[]{nx, ny});
+                }
+            }
+        }
+        
+        return -1;
+    }
+}
