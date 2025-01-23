a474aad9aaa1a697bc25beac59b68b87b7158d5b
오태현 <jeffreyoh121@gmail.com>
2025-01-23 17:22:16 +0900

    [level 2] Title: 큰 수 만들기, Time: 56.97 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,6 +1,23 @@
+import java.util.*;
+
 class Solution {
     public String solution(String number, int k) {
-        String answer = "";
-        return answer;
+        ArrayDeque<Character> ad = new ArrayDeque<>();
+        for (char c : number.toCharArray()) {
+            while (!ad.isEmpty() && ad.peekLast() < c && k > 0) {
+                ad.pollLast();
+                k--;
+            }
+            ad.add(c);
+        }
+        
+        while (k > 0) {
+            ad.pollLast();
+            k--;
+        }
+        
+        StringBuilder answer = new StringBuilder();
+        for (char c : ad) answer.append(c);
+        return answer.toString();
     }
 }
