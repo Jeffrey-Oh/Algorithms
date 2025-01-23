0c0e4fed9f3ce8c76e9a20c8c19852a44986931e
오태현 <jeffreyoh121@gmail.com>
2025-01-23 15:39:42 +0900

    [level 2] Title: 조이스틱, Time: 0.08 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860#) 
 
 ### 성능 요약
 
-메모리: 86.6 MB, 시간: 0.06 ms
+메모리: 81.7 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 19일 23:19:38
+2025년 01월 23일 15:39:39
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -4,27 +4,18 @@ class Solution {
     public int solution(String name) {
         int n = name.length();
         int answer = 0;
-
-        // 알파벳 변경 비용 계산
-        for (int i = 0; i < n; i++) {
-            char c = name.charAt(i);
+        for (char c : name.toCharArray()) {
             answer += Math.min(c - 'A', 'Z' - c + 1);
         }
-
-        // 커서 이동 최소 비용 계산
-        int minMove = n - 1; // 초기값: 끝까지 직진하는 경우
-        for (int i = 0; i < n; i++) {
-            // 연속된 A 구간 찾기
+        int min = n - 1;
+        for (int i=0; i<n; i++) {
             int next = i + 1;
             while (next < n && name.charAt(next) == 'A') {
                 next++;
             }
-            // (왼쪽으로 이동 후 돌아가기)와 (끝까지 직진)의 최소값 비교
             int move = i + n - next + Math.min(i, n - next);
-            minMove = Math.min(minMove, move);
+            min = Math.min(min, move);
         }
-
-        answer += minMove;
-        return answer;
+        return answer + min;
     }
 }
