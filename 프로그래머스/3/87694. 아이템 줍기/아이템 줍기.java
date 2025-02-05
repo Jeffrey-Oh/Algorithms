99d4406e92777c58a4272f482e1ca65795d2eb87
오태현 <jeffreyoh121@gmail.com>
2025-02-05 23:04:36 +0900

    [level 3] Title: 아이템 줍기, Time: 1.32 ms, Memory: 93.1 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -6,24 +6,27 @@ class Solution {
         int maxSize = 50 * scale + 2;
         boolean[][] map = new boolean[maxSize][maxSize];
         
+        // 테두리
         for (int[] rect : rectangle) {
             int x1 = rect[0] * scale, y1 = rect[1] * scale;
             int x2 = rect[2] * scale, y2 = rect[3] * scale;
-            for (int x = x1; x <= x2; x++) {
+            for (int x = x1; x<=x2; x++) {
                 map[x][y1] = true;
                 map[x][y2] = true;
             }
-            for (int y = y1; y <= y2; y++) {
+            
+            for (int y = y1; y<=y2; y++) {
                 map[x1][y] = true;
                 map[x2][y] = true;
             }
         }
         
+        // 내부
         for (int[] rect : rectangle) {
             int x1 = rect[0] * scale + 1, y1 = rect[1] * scale + 1;
             int x2 = rect[2] * scale - 1, y2 = rect[3] * scale - 1;
-            for (int x = x1; x <= x2; x++) {
-                for (int y = y1; y <= y2; y++) {
+            for (int x = x1; x<=x2; x++) {
+                for (int y = y1; y<=y2; y++) {
                     map[x][y] = false;
                 }
             }
@@ -31,7 +34,7 @@ class Solution {
         
         Deque<int[]> q = new ArrayDeque<>();
         boolean[][] visited = new boolean[maxSize][maxSize];
-        int[] dx = {0, 0, -1, 1};
+        int[] dx = {0, 0, 1, -1};
         int[] dy = {1, -1, 0, 0};
         
         int startX = characterX * scale;
@@ -43,8 +46,10 @@ class Solution {
         visited[startX][startY] = true;
         
         while (!q.isEmpty()) {
-            int[] current = q.pop();
-            int x = current[0], y = current[1], distance = current[2];
+            int[] current = q.poll();
+            int x = current[0];
+            int y = current[1];
+            int distance = current[2];
             
             if (x == targetX && y == targetY) {
                 return distance / scale;
@@ -54,7 +59,7 @@ class Solution {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 
-                if (nx >= 0 && ny >= 0 && nx < maxSize && ny < maxSize) {
+                if (nx >= 0 & nx < maxSize && ny >= 0 && ny < maxSize) {
                     if (map[nx][ny] && !visited[nx][ny]) {
                         visited[nx][ny] = true;
                         q.add(new int[]{nx, ny, distance + 1});
