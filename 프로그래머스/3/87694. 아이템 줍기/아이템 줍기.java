5a023b3c630ef2d767e04f6fa45eef22fe8f1346
오태현 <jeffreyoh121@gmail.com>
2025-02-06 17:16:01 +0900

    [level 3] Title: 아이템 줍기, Time: 0.74 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -10,6 +10,7 @@ class Solution {
         for (int[] rect : rectangle) {
             int x1 = rect[0] * scale, y1 = rect[1] * scale;
             int x2 = rect[2] * scale, y2 = rect[3] * scale;
+            
             for (int x = x1; x<=x2; x++) {
                 map[x][y1] = true;
                 map[x][y2] = true;
@@ -25,6 +26,7 @@ class Solution {
         for (int[] rect : rectangle) {
             int x1 = rect[0] * scale + 1, y1 = rect[1] * scale + 1;
             int x2 = rect[2] * scale - 1, y2 = rect[3] * scale - 1;
+            
             for (int x = x1; x<=x2; x++) {
                 for (int y = y1; y<=y2; y++) {
                     map[x][y] = false;
@@ -32,18 +34,18 @@ class Solution {
             }
         }
         
-        Deque<int[]> q = new ArrayDeque<>();
-        boolean[][] visited = new boolean[maxSize][maxSize];
-        int[] dx = {0, 0, 1, -1};
-        int[] dy = {1, -1, 0, 0};
-        
         int startX = characterX * scale;
         int startY = characterY * scale;
         int targetX = itemX * scale;
         int targetY = itemY * scale;
         
+        Deque<int[]> q = new ArrayDeque<>();
         q.add(new int[]{startX, startY, 0});
+        boolean[][] visited = new boolean[maxSize][maxSize];
         visited[startX][startY] = true;
+        
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
         
         while (!q.isEmpty()) {
             int[] current = q.poll();
@@ -59,15 +61,13 @@ class Solution {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 
-                if (nx >= 0 & nx < maxSize && ny >= 0 && ny < maxSize) {
-                    if (map[nx][ny] && !visited[nx][ny]) {
-                        visited[nx][ny] = true;
-                        q.add(new int[]{nx, ny, distance + 1});
-                    }
+                if (nx >= 0 && ny >= 0 && nx < maxSize && ny < maxSize && map[nx][ny] && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    q.add(new int[]{nx, ny, distance + 1});
                 }
             }
         }
         
-        return 0;
+        return -1;
     }
 }
