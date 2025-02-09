7e1e2c3bd4ab0f8bd3cd3ea530ef24fc3306d637
오태현 <jeffreyoh121@gmail.com>
2025-02-10 00:55:58 +0900

    [level 2] Title: 스킬트리, Time: 0.10 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,42 +1,23 @@
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        int answer = 0, n = skill.length();
+        int answer = 0;
         
         for (String st : skill_trees) {
-            String pattern = "^[^" + skill + "]*$";
-            if (st.matches(pattern)) {
-                answer++;
-                continue;
-            }
+            StringBuilder filteredSkillTree = new StringBuilder();
             
-            int position = -1, index = 0;
-            boolean[] visited = new boolean[n];
-            boolean check = true;
-            for (char c : skill.toCharArray()) {
-                int io = st.indexOf(c);
-                if (io > -1) {
-                    if (index == 0 && !visited[index]) {
-                        visited[index++] = true;
-                        position = io;
-                        continue;
-                    } else if (!visited[index - 1]) {
-                        check = false;
-                        break;
-                    }
-                    
-                    if (io > position) {
-                        visited[index] = true;
-                        position = io;
-                    } else {
-                        check = false;
-                        break;
-                    }
+            // 스킬 트리에서 필수 스킬만 추출
+            for (char c : st.toCharArray()) {
+                if (skill.indexOf(c) != -1) {
+                    filteredSkillTree.append(c);
                 }
-                index++;
             }
             
-            if (check) answer++;
+            // 필터링된 스킬 트리가 스킬의 선행 조건을 만족하는지 확인
+            if (skill.startsWith(filteredSkillTree.toString())) {
+                answer++;
+            }
         }
+        
         return answer;
     }
 }
